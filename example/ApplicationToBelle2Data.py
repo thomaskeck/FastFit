Fixed some bugs in c api concerning to move from 7d variance to 6d variance

example/ApplicationToBelle2Data.py
@@ -9,6 +9,7 @@ from PyFastFit import FastFit
 import numpy as np
 import pandas
 import matplotlib.pyplot as plt
+import scipy.stats
 
 if __name__ == '__main__':
 
@@ -21,6 +22,8 @@ if __name__ == '__main__':
         df['fastfit_dx'] = 0.0
         df['fastfit_dy'] = 0.0
         df['fastfit_dz'] = 0.0
+        
+        df['fastfit_chiProb'] = 0.0
 
         for i in range(n):
             df['fastfit_px_{}'.format(i)] = 0.0
@@ -41,9 +44,11 @@ if __name__ == '__main__':
                 momentum = np.array([decay['px_{}'.format(i)], decay['py_{}'.format(i)], decay['pz_{}'.format(i)]])
                 position = np.array([decay['dx_{}'.format(i)], decay['dy_{}'.format(i)], decay['dz_{}'.format(i)]])
                 variance = np.zeros((7, 7))
-                for j in range(7):
-                    for k in range(7):
-                        variance[j, k] = decay['momVertCovM{}{}_{}'.format(j, k, i)]
+
+                m = [4, 5, 6, 0, 1, 2]
+                for j in range(6):
+                    for k in range(6):
+                        variance[j, k] = decay['momVertCovM{}{}_{}'.format(m[j], m[k], i)]
                 fitter.setDaughter(i, charge, momentum, position, variance)
 
             fitter.fit(3)
@@ -52,6 +57,8 @@ if __name__ == '__main__':
             df.loc[index, 'fastfit_dx'] = vertex[0]
             df.loc[index, 'fastfit_dy'] = vertex[1]
             df.loc[index, 'fastfit_dz'] = vertex[2]
+            
+            df.loc[index, 'fastfit_chiProb'] = 1.0 - scipy.stats.chi2.cdf(fitter.getChi2(), fitter.getNDF())
 
             for i in range(n):
                 momentum = fitter.getDaughterMomentum(i)
@@ -88,4 +95,15 @@ if __name__ == '__main__':
         ax3.set_title("Z")
         (df['fastfit_dz'] - df['dz']).hist(bins=100, range=r, label='FastFit', alpha=0.3, ax=ax3, color='r')
         plt.savefig('KF_D{}.png'.format(n))
+        plt.clf()
+        
+        r = (0.0, 1.0)
+        f, (ax1, ax2) = plt.subplots(2, sharex=True, sharey=True)
+        f.suptitle("Chi2: Distribution and Deviation from KFit")
+        ax1.set_title("Chi2")
+        df['fastfit_chiProb'].hist(bins=100, range=r, label='FastFit', alpha=0.3, ax=ax1, color='r')
+        df['chiProb'].hist(bins=100, range=r, label='KFit', alpha=0.3, ax=ax1, color='b')
+        ax2.set_title("Chi")
+        (df['fastfit_chiProb'] - df['chiProb']).hist(bins=100, range=r, label='Deviation', alpha=0.3, ax=ax2, color='r')
+        plt.savefig('ChiProb_D{}.png'.format(n))
         plt.clf()

example/PythonExample.py
@@ -3,16 +3,17 @@
 
 from PyFastFit import FastFit
 import numpy as np
+import scipy.stats
 
 if __name__ == '__main__':
 
     fitter = FastFit.FastFit(2, 1.5)
-    fitter.setDaughter(0,  1, np.array([ 1.0, 0.0, 0.0]), np.array([-1.0, 0.0, 0.0]), np.diag([0.01] * 7))
-    fitter.setDaughter(1, -1, np.array([-1.0, 0.0, 0.0]), np.array([ 1.0, 0.0, 0.0]), np.diag([0.01] * 7))
+    fitter.setDaughter(0,  1, np.array([ 1.0, 0.0, 0.0]), np.array([-1.0, 0.0, 0.0]), np.diag([0.01] * 6))
+    fitter.setDaughter(1, -1, np.array([-1.0, 0.0, 0.0]), np.array([ 1.0, 0.0, 0.0]), np.diag([0.01] * 6))
     fitter.fit(3)
 
     print("Vertex", fitter.getVertex())
-    print("Chi2 / NDF", fitter.getChi2(), " / ", fitter.getNDF())
+    print("Chi2 / NDF", fitter.getChi2(), " / ", fitter.getNDF(), " => ", 1.0 - scipy.stats.chi2.cdf(fitter.getChi2(), fitter.getNDF()))
     print("Momentum Daughter 1", fitter.getDaughterMomentum(0))
     print("Momentum Daughter 2", fitter.getDaughterMomentum(1))
     print("Momentum Variance 1", fitter.getDaughterVariance(0))